0cfb7897cd9e92b5e8d6a35d6304963358391686
raddva <111165508+raddva@users.noreply.github.com>
2025-01-07 21:01:44 +0700

    Add resources

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/buttons', 'Home::buttons');
+$routes->get('/dropdowns', 'Home::dropdowns');
+$routes->get('/typography', 'Home::typography');
+$routes->get('/icons', 'Home::icons');
+$routes->get('/forms', 'Home::forms');
+$routes->get('/charts', 'Home::charts');
+$routes->get('/tables', 'Home::tables');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,41 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('admin/index');
+    }
+
+    public function buttons(): string
+    {
+        return view('admin/ui-features/buttons');
+    }
+
+    public function dropdowns(): string
+    {
+        return view('admin/ui-features/dropdowns');
+    }
+
+    public function typography(): string
+    {
+        return view('admin/ui-features/typography');
+    }
+
+    public function icons(): string
+    {
+        return view('admin/icons/mdi');
+    }
+    
+    public function forms(): string
+    {
+        return view('admin/forms/basic_elements');
+    }
+
+    public function charts(): string
+    {
+        return view('admin/charts/chartjs');
+    }
+
+    public function tables(): string
+    {
+        return view('admin/tables/basic-table');
     }
 }
